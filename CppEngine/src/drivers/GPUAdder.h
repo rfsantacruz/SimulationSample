eb7b1ef103e500c7b4ffd4b4d9c42c7545981a1c
rfsantacruz <rfsantacruz@gmail.com>
2015-04-16 01:27:01 -0300

    Initial Cuda Cpp Project

---
+++ CppEngine/src/drivers/GPUAdder.h
@@ -0,0 +1,20 @@
+/*
+ * GPUAdder.h
+ *
+ *  Created on: Apr 16, 2015
+ *      Author: rfsantacruz
+ */
+
+#ifndef GPUADDER_H_
+#define GPUADDER_H_
+
+namespace drivers {
+
+class GPUAdder {
+public:
+	GPUAdder();
+	virtual ~GPUAdder();
+};
+
+} /* namespace drivers */
+#endif /* GPUADDER_H_ */
